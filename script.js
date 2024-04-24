7903f7cbafcd05d62fb6722b90d634656ae0ce27
rafaseoanes <rafaseoanes9@gmail.com>
2024-04-24 11:58:37 -0500

    Password validation implemented

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hey")
+let password = document.getElementById("password")
+let checkPassword = document.getElementById("check-password")
+let errorMessage = document.getElementById("pw-error")
+
+let form = document.getElementById("mainForm")
+
+form.addEventListener("submit", (e)=>{
+
+    if(password.value !== checkPassword.value){
+        password.classList.toggle("noValid")
+        checkPassword.classList.toggle("noValid")
+        errorMessage.classList.toggle("hide")
+    }
+    e.preventDefault()
+})
+
